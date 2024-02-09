0bde8394475d43972386c3bfe62fb48384185880
Lucille Alvar <lucille.alvar@gmail.com>
2024-02-09 16:52:29 -0700

    On branch feature-FSV-21 Changes to be committed: 	modified:   src/components/Search-bar/Search-bar.scss 	modified:   src/pages/HomePage/HomePage.jsx 	new file:   src/pages/HomePage/HomePage.scss

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import SearchBar from '../../components/Search-bar/Search-bar';
+import React from "react";
+import './HomePage.scss';
+import SearchBar from "../../components/Search-bar/Search-bar";
+
+import { PiUserLight } from "react-icons/pi";
+import { BsFilterLeft } from "react-icons/bs";
 
 const HomePage = () => {
-    return (
-        <div>
-            Home Page
-           <SearchBar/>
+  return (
+    <div className="home">
+        <div className="home_top">
+        <div className="left">
+        <div className="left-user">
+          <PiUserLight />
         </div>
-    )
-}
+        <div className="left-greeting">
+          <p>Hey, User</p>
+        </div>
+      </div>
+      <div className="right">
+        <SearchBar className="right-searchbar"/>
+        <BsFilterLeft className="right-filter"/>
+      </div>
+        </div>
+        <div className="home_recommendation">
+            <h2>Recommended</h2>
+            <p>View all</p>
+        </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
