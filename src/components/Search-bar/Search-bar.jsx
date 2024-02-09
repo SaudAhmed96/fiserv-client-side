500d6667ba1d7a083c4e8ee2fc6e0edce1bcb185
Lucille Alvar <lucille.alvar@gmail.com>
2024-02-09 14:53:38 -0700

    On branch feature-FSV-30 Changes to be committed: 	new file:   src/components/Search-bar/Search-bar.jsx 	new file:   src/components/Search-bar/Search-bar.scss 	modified:   src/pages/HomePage/HomePage.jsx

---
+++ src/components/Search-bar/Search-bar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./Search-bar.scss";
+
+import { AiOutlineSearch } from "react-icons/ai";
+
+const SearchBar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleIconClick = (e) => {
+    e.preventDefault();
+    setActive(!active);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="search">
+      <form className="search-bar">
+        <input
+          type="text"
+          className={active ? "input active" : "input"}
+          placeholder="Search"
+        />
+        <button className="btn" type="submit" onClick={handleIconClick}>
+          <AiOutlineSearch className="icon" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
