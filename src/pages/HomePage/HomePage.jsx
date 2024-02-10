08fb6284f3ac345d5da2e8310a651bee822abc38
Lucille Alvar <lucille.alvar@gmail.com>
2024-02-10 10:53:59 -0700

    On branch feature-FSV-32 Changes to be committed: 	new file:   src/assets/images/Screenshot 2024-02-09 at 3.57.45 PM.png 	new file:   src/assets/images/Screenshot 2024-02-09 at 4.05.42 PM.png 	new file:   src/assets/images/Screenshot 2024-02-09 at 5.02.17 PM.png 	new file:   src/assets/images/Screenshot 2024-02-09 at 5.06.59 PM.png 	new file:   src/components/Promo-card/Promo-card.jsx 	new file:   src/components/Promo-card/Promo-card.scss 	modified:   src/components/Search-bar/Search-bar.scss 	new file:   src/components/WeekTop-card/WeekTop-card.jsx 	new file:   src/components/WeekTop-card/WeekTop-card.scss 	modified:   src/pages/HomePage/HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SearchBar from '../../components/Search-bar/Search-bar';
+import PromoCard from '../../components/Promo-card/Promo-card';
+import WeekTop from '../../components/WeekTop-card/WeekTop-card';
 
 const HomePage = () => {
     return (
         <div>
             Home Page
            <SearchBar/>
+           <PromoCard/>
+           <WeekTop/>
         </div>
     )
 }
