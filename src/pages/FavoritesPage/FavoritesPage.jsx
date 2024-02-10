638cf25eb909db4cd1eb2dfa0c2402c7b99cc17e
Saud Ahmed <saudahmed96x@gmail.com>
2024-02-09 22:35:34 -0500

    added basic layout fav page

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './FavoritesPage.scss'
 
 const FavoritesPage = () => {
     return (
-        <div>
-            <h1>FavoritesPage</h1>
+        <div className='favorites'>
+            <h1 className='favorites__heading'>My Favorites</h1>
+            <div className='favorites__filter'>
+                <h2 className='favorites__option'>
+                    <NavLink to={'/favorites'}>Meals</NavLink>
+                </h2>
+                <h2 className='favorites__option'>
+                    <NavLink to={'/'}>Restaurants</NavLink>
+                </h2>
+            </div>
+
+            <div className='favorites__body'>
+                <ul className='favorites__list'>
+                    <li className='favorites__item'>
+                        <NavLink className='favorites__link' to={'/'}>
+                            <div>h</div>
+                        </NavLink>
+                    </li>
+
+                </ul>
+
+            </div>
         </div>
     )
 }
