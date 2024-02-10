d9688d3d62b8727e232c5ab232432e6ab379cb10
Saud Ahmed <saudahmed96x@gmail.com>
2024-02-09 20:59:03 -0500

    changed wishlist to favorites

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -40,7 +40,7 @@ const Nav = () => {
                         onClick={() => { changeSelection("favorites") }}
                         to={'/favorites'}>
                         <GoHeart className='nav__icon' />
-                        {selected === "favorites" ? <p className='nav__text'>Wishlist</p> : <></>}
+                        {selected === "favorites" ? <p className='nav__text'>Favorites</p> : <></>}
                     </NavLink>
                 </li>
                 <li className='nav__item'>
